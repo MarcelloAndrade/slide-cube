b4f57829d4d0d7119f0d4795b4f3e3d07123af75
Marcello Andrade <marcellohfandrade@gmail.com>
2020-09-11 17:22:41 -0300

    add base ui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,7 +44,7 @@ public class PlayerController : MonoBehaviour {
     private void Update() {
         transform.position = Vector3.MoveTowards(transform.position, moveToPoint.position, speed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, moveToPoint.position) == 0) {
+        if (Vector3.Distance(transform.position, moveToPoint.position) < 0.3) {
             Axis direction = Axis.None;
             float moveY = 0f;
             float moveX = 0f;
